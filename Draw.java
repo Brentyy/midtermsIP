338a9e10f701f7afcf1c742b8daaae2e58afbd53
brentyy <brentrivera282@gmail.com>
2019-02-14 12:07:16 +0800

    number3

--- Draw.java
+++ Draw.java
@@ -6,140 +6,195 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.net.URL;
+import java.util.Random;
 
 public class Draw extends JComponent{
 
-	private BufferedImage image;
-	private BufferedImage backgroundImage;
-	public URL resource = getClass().getResource("run0.png");
+  private BufferedImage image;
+  private BufferedImage backgroundImage;
+  public URL resource = getClass().getResource("run0.png");
 
-	// circle's position
-	public int x = 30;
-	public int y = 30;
+  // circle's position
+  public int x = 30;
+  public int y = 30;
+    public int height = 0;
+    public int width = 0;
+  // animation states
+  public int state = 0;
 
-	// animation states
-	public int state = 0;
+    public Random randomizer;
 
-	Monster monster1;
-	Monster monster2;
-	Monster monster3;
-	Monster monster4;
-	Monster monster5;
+    public int enemyCount = 0;
+    Monster[] monsters = new Monster[10];
 
-	public Draw(){
-		monster1 = new Monster(200, 200);
-		monster2 = new Monster(300, 200);
-		monster3 = new Monster(350, 200);
-		monster4 = new Monster(400, 200);
-		monster5 = new Monster(250, 200);
+   public Draw(){
+       randomizer = new Random();
+       spawnEnemy();
 
-		try{
-			image = ImageIO.read(resource);
-			backgroundImage = ImageIO.read(getClass().getResource("background.jpg"));
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-	}
+    try{
+      image = ImageIO.read(resource);
+      backgroundImage = ImageIO.read(getClass().getResource("background.jpg"));
+    }
+    catch(IOException e){
+      e.printStackTrace();
+    }
 
-	public void reloadImage(){
-		state++;
+       height = image.getHeight();
+     width = image.getWidth();
+  }
 
-		if(state == 0){
-			resource = getClass().getResource("run0.png");
-		}
-		else if(state == 1){
-			resource = getClass().getResource("run1.png");
-		}
-		else if(state == 2){
-			resource = getClass().getResource("run2.png");
-		}
-		else if(state == 3){
-			resource = getClass().getResource("run3.png");
-		}
-		else if(state == 4){
-			resource = getClass().getResource("run4.png");
-		}
-		else if(state == 5){
-			resource = getClass().getResource("run5.png");
-			state = 0;
-		}
+  public void reloadImage(){
+    state++;
 
-		try{
-			image = ImageIO.read(resource);
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-	}
+    if(state == 0){
+      resource = getClass().getResource("run0.png");
+    }
+    else if(state == 1){
+      resource = getClass().getResource("run1.png");
+    }
+    else if(state == 2){
+      resource = getClass().getResource("run2.png");
+    }
+    else if(state == 3){
+      resource = getClass().getResource("run3.png");
+    }
+    else if(state == 4){
+      resource = getClass().getResource("run4.png");
+    }
+    else if(state == 5){
+      resource = getClass().getResource("run5.png");
+      state = 0;
+    }
 
-	public void attackAnimation(){
-		Thread thread1 = new Thread(new Runnable(){
-			public void run(){
-				for(int ctr = 0; ctr < 5; ctr++){
-					try {
-						if(ctr==4){
-							resource = getClass().getResource("run0.png");
-						}
-						else{
-							resource = getClass().getResource("attack"+ctr+".png");
-						}
-						
-						try{
-							image = ImageIO.read(resource);
-						}
-						catch(IOException e){
-							e.printStackTrace();
-						}
-				        repaint();
-				        Thread.sleep(100);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		});
-		thread1.start();
-	}
+    try{
+      image = ImageIO.read(resource);
+    }
+    catch(IOException e){
+      e.printStackTrace();
+    }
+  }
+    public void spawnEnemy(){
+     if(enemyCount < 10){
+        monsters[enemyCount] = new Monster(randomizer.nextInt(500), randomizer.nextInt(500), this);
+        enemyCount++;
+      }
+   }
+    public void attackAnimation(){
+      Thread thread1 = new Thread(new Runnable(){
+        public void run(){
+         for(int ctr = 0; ctr < 5; ctr++){
+           try {
+             if(ctr==4){
+               resource = getClass().getResource("run0.png");
+             }
+             else{
+               resource = getClass().getResource("attack"+ctr+".png");
+             }
+             
+             try{
+               image = ImageIO.read(resource);
+             }
+             catch(IOException e){
+               e.printStackTrace();
+             }
+                 repaint();
+                 Thread.sleep(100);
+           } catch (InterruptedException e) {
+             e.printStackTrace();
+           }
+         }
+         for(int x=0; x<monsters.length; x++){
+           if(monsters[x]!=null){
+             if(monsters[x].contact){
+               monsters[x].life = monsters[x].life - 10;
+              }
+           }
+          }
 
-	public void attack(){
-		attackAnimation();
-	}
+        }
+     });
+     thread1.start();
+    }
 
-	public void moveUp(){
-		y = y - 5;
-		reloadImage();
-		repaint();
-	}
+  public void attack(){
+    attackAnimation();
+    checkCollision();
+  }
 
-	public void moveDown(){
-		y = y + 5;
-		reloadImage();
-		repaint();
-	}
+  public void moveUp(){
+    y = y - 5;
+    reloadImage();
+    repaint();
+  }
 
-	public void moveLeft(){
-		x = x - 5;
-		reloadImage();
-		repaint();
-	}
+  public void moveDown(){
+    y = y + 5;
+    reloadImage();
+    repaint();
+  }
 
-	public void moveRight(){
-		x = x + 5;
-		reloadImage();
-		repaint();
-	}
-	
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-		g.setColor(Color.YELLOW);
-		g.drawImage(backgroundImage, 0, 0, this);
-		g.drawImage(image, x, y, this);
+  public void moveLeft(){
+    x = x - 5;
+    reloadImage();
+    repaint();
+  }
 
-		g.drawImage(monster1.image, monster1.xPos, monster1.yPos, this);
-		g.drawImage(monster2.image, monster2.xPos, monster2.yPos, this);
-		g.drawImage(monster3.image, monster3.xPos, monster3.yPos, this);
-		g.drawImage(monster4.image, monster4.xPos, monster4.yPos, this);
-		g.drawImage(monster5.image, monster5.xPos, monster5.yPos, this);
-	}
+  public void moveRight(){
+    x = x + 5;
+    reloadImage();
+    repaint();
+  }
+  
+    public void checkCollision(){
+      int xChecker = x + width;
+      int yChecker = y;
+
+      for(int x=0; x<monsters.length; x++){
+        boolean collideX = false;
+        boolean collideY = false;
+
+        if(monsters[x]!=null){
+          monsters[x].contact = false;
+
+          if(yChecker > monsters[x].yPos){
+            if(yChecker-monsters[x].yPos < monsters[x].height){
+             collideY = true;
+            }
+          }
+         else{
+            if(monsters[x].yPos - yChecker < monsters[x].height){
+             collideY = true;
+            }
+         }
+
+          if(xChecker > monsters[x].xPos){
+            if(xChecker-monsters[x].xPos < monsters[x].width){
+             collideX = true;
+           }
+          }
+         else{
+           if(monsters[x].xPos - xChecker < 5){
+             collideX = true;
+           }
+         }
+        }
+
+       if(collideX && collideY){
+         System.out.println("collision!");
+         monsters[x].contact = true;
+         }
+      }
+    }
+  public void paintComponent(Graphics g){
+    
+    super.paintComponent(g);
+    g.drawImage(backgroundImage, 0, 0, this);
+  g.drawImage(image, x, y, this);
+    for(int c = 0; c < monsters.length; c++){
+      if(monsters[c]!=null){
+        g.drawImage(monsters[c].image, monsters[c].xPos, monsters[c].yPos, this);
+        g.fillRect(monsters[c].xPos+7, monsters[c].yPos, monsters[c].life, 2);
+       }
+      }
+  }
 }
